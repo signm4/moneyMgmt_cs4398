7b11819e1dfc33e12c1707d22c94e56a3a4540b6
signm4 <signm3@gmail.com>
2023-02-21 13:03:09 -0600

    made login page

--- project.py
+++ project.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required
 
 app = Flask(__name__)
 
-@app.route('/')
+# @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
 
