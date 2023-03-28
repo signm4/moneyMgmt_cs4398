17991d6c71a3100d78bdf103755393e28c2095be
signm4 <signm3@gmail.com>
2023-03-28 12:20:18 -0500

    updated project.py

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # moneyMgmt_cs4398
+
+to run: FLASK_APP=project flask run
+pip3 install flask-login
--- project.py
+++ project.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
 from flask_login import LoginManager, UserMixin, login_user, current_user, login_required
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/sulemanm/Documents/SWE Project/moneyMgmt_cs4398/login.db'
+app.config['SECRET_KEY'] = 'thisissecret'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique = True)
+
+    
+
 
 # @app.route('/')
 @app.route('/<name>')
