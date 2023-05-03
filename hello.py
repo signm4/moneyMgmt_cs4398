5c14e8e4b5aef92f0e6066cf781ed15ce24584ea
signm4 <signm3@gmail.com>
2023-05-03 17:04:50 -0500

    user sessions working

--- hello.py
+++ hello.py
@@ -1,112 +1,138 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from Backend_Functions import addItem1
 
-# import sqlite3
-
-# conn = sqlite3.connect('mydatabase.db')
-# c = conn.cursor()
-
-# c.execute('SELECT * FROM users')
-# users = c.fetchall()
-
-# for user in users:
-#     print(user)
 
 
 app = Flask(__name__)
-# app.secret_key = 'mysecretkey'
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def login():
-    # conn = sqlite3.connect('mydatabase.db')
-    # c = conn.cursor()
-    
-    # c.execute('SELECT * FROM users')
-    # users = c.fetchall()
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-# @app.route('/signup', methods=['GET', 'POST'])
-# def do_signup():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         conn = sqlite3.connect('mydatabase.db')
-#         c = conn.cursor()
-#         c.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-#         conn.commit()
-#         conn.close()
-#         session['username'] = username
-#         return redirect('/')
-#     else:
-#         return '''
-#             <form method="post">
-#                 <input type="text" name="username" placeholder="Username">
-#                 <input type="password" name="password" placeholder="Password">
-#                 <button type="submit">Sign Up</button>
-#             </form>
-#         '''
-
 @app.route('/register', methods=['POST'])
 def do_signup():
     username = request.form['username']
     password = request.form['password']
+    income = 0
+    expenses = 0
+    if add_user_data(username, password, income, expenses):
+        flash('Account created successfully')
+        return redirect('/')
+    else:
+        flash('Username already exists')
+        return redirect('/register')
+    
+# old working signup 
+# @app.route('/register', methods=['POST'])
+# def do_signup():
+#     username = request.form['username']
+#     password = request.form['password']
 
-    # save the username and password to a file
-    with open('users.txt', 'a') as file:
-        file.write(f"{username}:{password}\n")
+#     # save the username and password to a file
+#     with open('users.txt', 'a') as file:
+#         file.write(f"{username}:{password}:{0}:{0}\n")
 
-    return render_template('login.html', username=username)
+#     return render_template('login.html', username=username)
+
+def get_all_usernames():
+    # Open the user database file and read all lines
+    with open('users.txt', 'r') as f:
+        lines = f.readlines()
+
+    # Extract the username from each line and return as a list
+    usernames = [line.split(',')[0].strip() for line in lines]
+    return usernames
+
+def check_user_credentials(username, password):
+    user_data = get_user_data(username)
+    print(user_data) #debugging
+    if user_data is not None and user_data['password'] == password:
+        session['username'] = username
+        session['income'] = user_data['income']
+        session['expenses'] = user_data['expenses']
+        return True
+    return False
+
+
+# def add_user_data(username, password, income, expenses):
+#     # Check if the username is available
+#     if username in get_all_usernames():
+#         return False
+
+#     # Create a new user data file
+#     with open(f'{username}.txt', 'w') as f:
+#         f.write(f'{password}\n')
+#         f.write(f'{income}\n')
+#         f.write(f'{expenses}\n')
+    
+#     return True
+
+def add_user_data(username, password, income, expenses):
+    # Open the user database file in append mode
+    with open('users.txt', 'a') as f:
+        # Write the user data to a new line in the file
+        f.write(f"{username}:{password}{income}:{expenses}\n")
+
+
+def get_user_data(username):
+    print ("Current user: ",username) #debugging
+    with open('users.txt', 'r') as f:
+        for line in f:
+            stored_username, stored_password, stored_income, stored_expenses = line.strip().split(':')
+            if stored_username == username:
+                return {'password':str(stored_password), 'income': float(stored_income), 'expenses': float(stored_expenses)}
+    return None
 
 # old login method via text file 
 @app.route('/login', methods=['POST'])
 def do_login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
     # check if the username and password are correct by reading the file
-    with open('users.txt', 'r') as file:
-        users = file.readlines()
+    # with open('users.txt', 'r') as file:
+    #     users = file.readlines()
 
-    for user in users:
-        u, p = user.strip().split(':')
-        if u == username and p == password:
-            return render_template('index.html', username=request.form['username'])
+    # for user in users:
+    #     u, p = user.strip().split(':')
+    #     if u == username and p == password:
+    #         return render_template('index.html', username=request.form['username'])
 
-    return "Invalid username or password."
+    # return "Invalid username or password."
+        if check_user_credentials(username, password):
+            session['username'] = username
+            return redirect('/index')
+        else:
+            return 'Invalid username or password'
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def do_login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         print("USERNAME ", username)
-#         print("PASSWORD ", password)
-#         conn = sqlite3.connect('mydatabase.db')
-#         c = conn.cursor()
-#         c.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
-#         user = c.fetchone()
-#         conn.close()
-#         if user is not None:
-#             session['username'] = user[1]
-#             return redirect('/')
-#         else:
-#             return 'Invalid username or password'
-#     else:
-#         return '''
-#             <form method="post">
-#                 <input type="text" name="username" placeholder="Username">
-#                 <input type="password" name="password" placeholder="Password">
-#                 <button type="submit">Log In</button>
-#             </form>
-#         '''
+    # Show the login form
+    return render_template('login.html')
 
-@app.route('/')
+@app.route('/logout')
+def logout():
+    # Clear the session data
+    session.clear()
+
+    # Redirect the user to the login page
+    return redirect(('/'))
+
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    if 'username' not in session:
+        return redirect('/login')
+
+    # Get the user's data from the database
+    username = session['username']
+    user_data = get_user_data(username)
+
+    # Render the template with the user's data
+    return render_template('index.html', username=username, user_data=user_data)
 
 @app.route('/home')
 def go_home():
@@ -129,7 +155,8 @@ def calculate():
 
 @app.route('/expenses', methods=['GET', 'POST'])
 def expenses():
-
+    username = session['username']
+    user_data = get_user_data(username)
     if request.method == 'POST':
         # name = request.form['name']
         amount = request.form['amount']
@@ -140,22 +167,26 @@ def expenses():
         return redirect('/expenses')
     else:
         
-        return render_template('expenses.html', expenses=expenses)
+        return render_template('expenses.html', expenses=expenses, username = username)
     
 @app.route('/spend', methods = ['GET', 'POST'])
 def spend():
+    username = session['username']
     if request.method == 'POST':
         amount = request.form['amount']
 
-    return render_template('spend.html')
+    return render_template('spend.html', username = username)
 
 @app.route('/income', methods = ['GET', 'POST'])
 def income():
+    username = session['username']
     if request.method == 'POST':
         amount = request.form['amount']
         frequency = request.form['frequency']
 
-    return render_template('income.html')
+    return render_template('income.html', username = username)
+
+
     
 if __name__ == '__main__':
     app.run()
